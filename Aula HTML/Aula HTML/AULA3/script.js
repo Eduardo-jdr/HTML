89d7b5924369bcf0b9cf9bdce4090d726f9f9fb2
Eduardo-jdr <caedjdr@gmail.com>
2025-05-07 12:23:34 -0300

    Add files via upload

---
+++ Aula HTML/Aula HTML/AULA3/script.js
@@ -0,0 +1,19 @@
+//console.log(1 + 2)
+
+//VARIAVEIS
+//Alocando uma informação no espaço da memória
+const idade = 43
+console.log(idade, typeof idade)
+
+var comida = "Macarrão"
+console.log(`Minha comida preferida é ${comida}`)
+
+var comida
+let hobby
+const carro = undefined
+console.log(hobby)
+
+var nome = "Carlos"
+var sobrenome = "Rocha"
+console.log("Meu nome é, ", nome, sobrenome)
+
